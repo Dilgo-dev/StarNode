827f1a7b4c671dcc6bbbf1491a26173349f5e291
OOOSNA <Dilgopierre@gmail.com>
2023-11-04 13:54:48 +0100

    feat(starnode): Return value is now send according to is type

--- src/starnode.ts
+++ src/starnode.ts
@@ -6,33 +6,37 @@ type ResponseHttp = http.ServerResponse<http.IncomingMessage> & {
 
 type RequestHttp = http.IncomingMessage;
 
-type Callback = (req: RequestHttp, res: Response) => any;
+type Callback = (req: Request, res: Response) => any;
 
+type TypeRequest = 'GET' | 'POST';
+
+type SNode = {
+    [key: string]: [Callback, TypeRequest]
+};
 interface Route {
     type: string,
     callback: Callback;
 }
 
 export class Request {
-    res: ResponseHttp;
-    statusCode: number = 200;
+    req: RequestHttp;
+    headers: http.IncomingHttpHeaders;
+    body: any;
 
-    constructor(res: ResponseHttp) {
-        this.res = res;
-    }
-
-    public status(code: number): Response {
-        this.statusCode = code;
-        return this;
-    }
-
-    public send(text: string) {
-        this.res.writeHead(this.statusCode, { 'Content-Type': 'text/plain' })
-        this.res.end(text);
-        return;
+    constructor(req: RequestHttp) {
+        this.req = req;
+        this.headers = req.headers;
+        switch (this.headers['content-type']) {
+            case 'application/json': 
+                req.on('data', (chunk) => {
+                    this.body = JSON.parse(chunk);
+                });
+                break; 
+            default:
+                break;
+        }
     }
 }
-
 export class Response {
     res: ResponseHttp;
     statusCode: number = 200;
@@ -47,49 +51,62 @@ export class Response {
     }
 
     public send(text: string) {
-        this.res.writeHead(this.statusCode, { 'Content-Type': 'text/plain' })
+        this.res.writeHead(this.statusCode, { 'Content-Type': 'text/html; charset=utf-8' })
         this.res.end(text);
+        return;
+    }
+
+    public json(jsonObject: object) {
+        try {
+            const jsonString: string = JSON.stringify(jsonObject);
+            this.res.writeHead(this.statusCode, { 'Content-Type': 'application/json' })
+            this.res.end(jsonString);
+        } catch (err) {
+            console.log(err);
+        }
         return;
     }
 }
 
-export default class StarNode {
+export default class StarServer {
     private routes: Map<string, Route> = new Map();
 
-    constructor() {};
+    constructor(routes: SNode) {
+        Object.keys(routes).forEach((route: string) => {
+            this.routes.set(route, {
+                type: routes[route][1],
+                callback: routes[route][0]
+            });
+        });
+    };
 
-    public get(path: string, callback: Callback) {
-        this.routes.set( path, { type: "GET", callback } );
-    }
-
-    private err(res: ResponseHttp, code: number, error: string) {
-        res.writeHead(code, { 'Content-Type': 'text/plain' });
-        return res.end(error);
+    private err(res: ResponseHttp, code: number, message: string) {
+        res.writeHead(code, { 'Content-Type': 'application/json' });
+        return res.end(message);
     }
 
     public listen(port: number, callback: () => any) {
         const server = http.createServer((req, res) => {
             if (!req.url) return this.err(res, 404, "No url");
+            console.log(req.url);
+            console.log(this.routes);
             const route = this.routes.get(req.url);
-            if (!route) return this.err(res, 404, "Not Found");
+            console.log(route);
+            if (!route || route.type !== req.method) return this.err(res, 404, "Not Found");
 
-            route.callback(req, new Response(res));
+            const returnValue: any = route.callback(new Request(req), new Response(res));
+            switch (typeof returnValue) {
+                case 'object':
+                    new Response(res).json(returnValue);
+                    break;
+                case 'string':
+                    new Response(res).send(returnValue);
+                    break;
+                default:
+                    break;
+            }
         });
 
         server.listen(port, callback);
     }
 }
-
-//const server = http.createServer((req, res) => {
-    //if (req.method === 'GET' && req.url === '/api') {
-        //res.writeHead(200, { 'Content-Type': 'application/json' });
-        //res.end(JSON.stringify({ message: 'Hello from the API!' }));
-    //} else {
-        //res.writeHead(404, { 'Content-Type': 'text/plain' });
-        //res.end('Not Found');
-    //}
-//});
-
-//server.listen(port, () => {
-    //console.log(`Server is listening on port ${port}`);
-//});
